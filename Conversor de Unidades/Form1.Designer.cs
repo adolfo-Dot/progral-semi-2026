3436d6808cf8683da2b9e4037e2a9a6f58f84db0
Angel Jimenez <aadolfojimenezjovel@gmail.com>
2026-08-26 10:12:26 -0600

    Lab 2

--- Conversor de Unidades/Form1.Designer.cs
+++ Conversor de Unidades/Form1.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.lblOpciones.AutoSize = true;
             this.lblOpciones.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpciones.Location = new System.Drawing.Point(136, 36);
+            this.lblOpciones.Location = new System.Drawing.Point(102, 29);
+            this.lblOpciones.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOpciones.Name = "lblOpciones";
-            this.lblOpciones.Size = new System.Drawing.Size(127, 31);
+            this.lblOpciones.Size = new System.Drawing.Size(97, 23);
             this.lblOpciones.TabIndex = 0;
             this.lblOpciones.Text = "Opciones:";
             this.lblOpciones.Click += new System.EventHandler(this.label1_Click);
@@ -57,9 +58,10 @@
             // 
             this.lblDe.AutoSize = true;
             this.lblDe.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDe.Location = new System.Drawing.Point(97, 108);
+            this.lblDe.Location = new System.Drawing.Point(73, 88);
+            this.lblDe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDe.Name = "lblDe";
-            this.lblDe.Size = new System.Drawing.Size(52, 31);
+            this.lblDe.Size = new System.Drawing.Size(39, 23);
             this.lblDe.TabIndex = 1;
             this.lblDe.Text = "De:";
             this.lblDe.Click += new System.EventHandler(this.lblDe_Click);
@@ -68,9 +70,10 @@
             // 
             this.lblA.AutoSize = true;
             this.lblA.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblA.Location = new System.Drawing.Point(383, 108);
+            this.lblA.Location = new System.Drawing.Point(287, 88);
+            this.lblA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblA.Name = "lblA";
-            this.lblA.Size = new System.Drawing.Size(38, 31);
+            this.lblA.Size = new System.Drawing.Size(28, 23);
             this.lblA.TabIndex = 2;
             this.lblA.Text = "A:";
             // 
@@ -78,9 +81,10 @@
             // 
             this.lblCantidad.AutoSize = true;
             this.lblCantidad.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCantidad.Location = new System.Drawing.Point(97, 217);
+            this.lblCantidad.Location = new System.Drawing.Point(73, 176);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(123, 31);
+            this.lblCantidad.Size = new System.Drawing.Size(95, 23);
             this.lblCantidad.TabIndex = 3;
             this.lblCantidad.Text = "Cantidad:";
             // 
@@ -88,9 +92,10 @@
             // 
             this.lblRespuestas.AutoSize = true;
             this.lblRespuestas.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRespuestas.Location = new System.Drawing.Point(111, 307);
+            this.lblRespuestas.Location = new System.Drawing.Point(83, 249);
+            this.lblRespuestas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRespuestas.Name = "lblRespuestas";
-            this.lblRespuestas.Size = new System.Drawing.Size(155, 31);
+            this.lblRespuestas.Size = new System.Drawing.Size(117, 23);
             this.lblRespuestas.TabIndex = 4;
             this.lblRespuestas.Text = "Respuesta: ?";
             // 
@@ -104,49 +109,55 @@
             "Longitud",
             "Almacenamiento",
             "Tiempo"});
-            this.cdbOpciones.Location = new System.Drawing.Point(284, 41);
+            this.cdbOpciones.Location = new System.Drawing.Point(213, 33);
+            this.cdbOpciones.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cdbOpciones.Name = "cdbOpciones";
-            this.cdbOpciones.Size = new System.Drawing.Size(208, 24);
+            this.cdbOpciones.Size = new System.Drawing.Size(157, 21);
             this.cdbOpciones.TabIndex = 5;
             // 
             // cdbDe
             // 
             this.cdbDe.FormattingEnabled = true;
-            this.cdbDe.Location = new System.Drawing.Point(168, 108);
+            this.cdbDe.Location = new System.Drawing.Point(126, 88);
+            this.cdbDe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cdbDe.Name = "cdbDe";
-            this.cdbDe.Size = new System.Drawing.Size(121, 24);
+            this.cdbDe.Size = new System.Drawing.Size(92, 21);
             this.cdbDe.TabIndex = 6;
             // 
             // cdbA
             // 
             this.cdbA.FormattingEnabled = true;
-            this.cdbA.Location = new System.Drawing.Point(446, 113);
+            this.cdbA.Location = new System.Drawing.Point(334, 92);
+            this.cdbA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cdbA.Name = "cdbA";
-            this.cdbA.Size = new System.Drawing.Size(121, 24);
+            this.cdbA.Size = new System.Drawing.Size(92, 21);
             this.cdbA.TabIndex = 7;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(466, 182);
+            this.btnCalcular.Location = new System.Drawing.Point(361, 142);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(134, 74);
+            this.btnCalcular.Size = new System.Drawing.Size(100, 60);
             this.btnCalcular.TabIndex = 8;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
             // 
             // txt
             // 
-            this.txt.Location = new System.Drawing.Point(218, 224);
+            this.txt.Location = new System.Drawing.Point(164, 182);
+            this.txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt.Name = "txt";
-            this.txt.Size = new System.Drawing.Size(100, 22);
+            this.txt.Size = new System.Drawing.Size(76, 20);
             this.txt.TabIndex = 9;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(375, 365);
+            this.button1.Location = new System.Drawing.Point(281, 297);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 58);
+            this.button1.Size = new System.Drawing.Size(88, 47);
             this.button1.TabIndex = 10;
             this.button1.Text = "Salir";
             this.button1.UseVisualStyleBackColor = true;
@@ -154,9 +165,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txt);
             this.Controls.Add(this.btnCalcular);
@@ -168,6 +179,7 @@
             this.Controls.Add(this.lblA);
             this.Controls.Add(this.lblDe);
             this.Controls.Add(this.lblOpciones);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
